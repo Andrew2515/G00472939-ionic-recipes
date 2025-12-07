996e11d084121663142b76ed419aa6c63aca4143
Andrew Walker <andrewadrianwalker@yahoo.ie>
2025-12-07 10:47:05 +0000

    Cleaned project before building new pages again

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+
+
 
 export const routes: Routes = [
   {
@@ -11,7 +14,7 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'recipe-details',
+    path: 'recipe-details/:id',
     loadComponent: () => import('./recipe-details/recipe-details.page').then( m => m.RecipeDetailsPage)
   },
   {
