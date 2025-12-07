80cb6421ec8ed717f8c3a27842ba53e1559ed6d0
Andrew Walker <andrewadrianwalker@yahoo.ie>
2025-12-07 10:52:45 +0000

    Added new pages and services for new build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,4 +25,16 @@ export const routes: Routes = [
     path: 'favourites',
     loadComponent: () => import('./favourites/favourites.page').then( m => m.FavouritesPage)
   },
+  {
+    path: 'recipe-details',
+    loadComponent: () => import('./recipe-details/recipe-details.page').then( m => m.RecipeDetailsPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'favourites',
+    loadComponent: () => import('./favourites/favourites.page').then( m => m.FavouritesPage)
+  },
 ];
