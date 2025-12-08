eab8a7c80851b3d8b1254f323579533da29d1f7e
Andrew Walker <andrewadrianwalker@yahoo.ie>
2025-12-08 11:38:28 +0000

    Created Favourites Page that shows saved recipes and Details navigation

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { FavouritesService } from '../services/favourites';
+import { RecipeSummary } from '../services/recipe';
+
+/*
+  This page displays all recipes the user marked as favourites.
+  The list is loaded from FavouritesService, which stores the data
+  using Ionic Storage so it stays after the app is closed.
+*/
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
-  styleUrls: ['./favourites.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  styleUrls: ['./favourites.page.scss']
 })
 export class FavouritesPage implements OnInit {
 
-  constructor() { }
+  // List of favourite recipes to show in the UI.
+  favouritesList: RecipeSummary[] = [];
+
+  constructor(
+    private favService: FavouritesService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    // Load all saved favourites from the service.
+    this.favouritesList = this.favService.getAll();
   }
 
+  // Navigate to the recipe details screen for this recipe.
+  openDetails(id: number) {
+    this.router.navigate(['/recipe-details', id]);
+  }
 }
