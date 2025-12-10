bc33adf455e519554abd5718fada2dc443d6dbcf
Andrew Walker <andrewadrianwalker@yahoo.ie>
2025-12-10 11:44:12 +0000

    Added provideHttpClient and corrected bootstrapApplication setup to fix blank screen and HttpClient provider error.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules
+} from '@angular/router';
+
+import { provideHttpClient } from '@angular/common/http';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-import { provideHttpClient } from '@angular/common/http';
-
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideHttpClient(),
+    provideHttpClient(),   // <<<<<< 🔥 REQUIRED 🔥
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
+
 
 
